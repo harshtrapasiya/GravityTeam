64d351322f878d2bd888ec5ef2898e77572f1058
harsh <jm@rudraitech.co.in>
2023-08-18 18:24:59 +0530

    18/08/2023

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import MarketMaking from './img/Market_Making.png'
+import Exchange from './img/exchange-.png'
+import Background from './img/background.png'
+import arrow from './img/arrow.svg'
 
 export default function Home() {
+
+
+
   return (
     <div>
       <div className='hero_section'>
@@ -10,6 +17,92 @@ export default function Home() {
         <Link className='get_btn'>Get in touch</Link>
       </div>
 
+      <div className='Aboutteam_Content'>
+        <h1>About Gravity Team </h1>
+        <p>At Gravity Team, we are on the mission to balance the supply and demand across crypto markets worldwide. We are a crypto native market maker founded by traders, developers, and innovators who are strong believers and supporters of the future of decentralization and digital assets.</p>
+      </div>
+
+      <div className='container_About'>
+        <div className='About_section'>
+          <div className='about_items'>
+            <h2>~$100 billion</h2>
+            <p>cumulative trading
+              volume to date</p>
+          </div>
+          <div className='about_items'>
+            <h2>0.8%</h2>
+            <p>of the global crypto spot trading volume</p>
+          </div>
+          <div className='about_items'>
+            <h2>~30</h2>
+            <p>Gravity Teammates
+              (& growing)</p>
+          </div>
+          <div className='about_items'>
+            <h2>25+</h2>
+            <p>leading global and local crypto exchanges</p>
+          </div>
+          <div className='about_items'>
+            <h2>2017</h2>
+            <p>start, crypto-natives</p>
+          </div>
+          <div className='about_items'>
+            <h2>1,200+</h2>
+            <p>crypto-asset pairs</p>
+          </div>
+          <div className='about_items'>
+            <h2>24/7</h2>
+            <p>liquidity</p>
+          </div>
+          <div className='about_items'>
+            <h2>5 billion+</h2>
+            <p>trades done to date</p>
+          </div>
+
+        </div>
+      </div>
+
+      <div className='Aboutteam_Content'>
+        <h1>Crypto Market Making </h1>
+        <p>We are a global crypto liquidity provider and algorithmic market maker. We trade digital assets listed on Centralized Exchanges in over 15 countries worldwide.</p>
+      </div>
+
+      <div className='container_About'>
+
+        <div className='crypto_section'>
+          <div className='bg_items'>
+            <img src={Background} alt="" className='img-fluid' />
+          </div>
+          <div className='crypto_Contant'>
+            <div className='MarketMaking_contant  order-1 order-lg-0'>
+              <h2>Market Making for Crypto Projects</h2>
+              <b>Accelerate your token’s journey by boosting its liquidity</b>
+              <p>We invest in building long-term, sustainable relationships and support our projects in their growth journey with our services, industry expertise and network.</p>
+              <Link className='LearnMore_btn'>Learn More <img src={arrow} alt="" /></Link>
+            </div>
+
+            <div className='order-0 order-lg-1 '>
+              <img src={MarketMaking} alt="" className='img-fluid' />
+            </div>
+          </div>
+
+          <div className='crypto_Contant'>
+            <div>
+              <img src={Exchange} alt="" className='img-fluid' />
+            </div>
+            <div className='MarketMaking_contant'>
+              <h2>Market Making for Crypto Exchanges</h2>
+              <b>Attract more traders and projects with deep order books & liquidity</b>
+              <p>Our world-class market making services are proven to help local and emerging exchanges win traders and gain market-leading positions of up to 90% market dominance.</p>
+              <Link className='LearnMore_btn'>Learn More <img src={arrow} alt="" /></Link>
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+
     </div>
   )
 }
+
--- src/Navbar.js
+++ src/Navbar.js
@@ -9,41 +9,49 @@ import { MdClose } from 'react-icons/md';
 export default function Navbar() {
     const [menu, setmenu] = useState(false)
 
+    if (menu === true) {
+        document.body.classList.add('overflow-hidden')
+    } if (menu === false) {
+        document.body.classList.remove('overflow-hidden')
+    }
+
     return (
         <div>
-            <div className={`blck_layers  ${menu === true? 'd-block':'d-none'}`} onClick={()=> setmenu(!menu)}></div>
-            <div className='Navbar_Section'>
-                <Link className='Logo_section'>
-                    <img src={Logo} alt="" className='img-fluid' />
-                </Link>
+            <div className={`blck_layers  ${menu === true ? 'd-block' : 'd-none'}`} onClick={() => setmenu(!menu)}></div>
+            <div className='Topbar'>
+                <div className='Navbar_Section'>
+                    <Link className='Logo_section'>
+                        <img src={Logo} alt="" className='img-fluid' />
+                    </Link>
 
-                <ul className={`Navbar_ul ${menu === true ? 'active' : ''}`}>
-                    <li className='Nav_items'>
-                        <Link className='Nav_link'>About Us</Link>
-                    </li>
+                    <ul className={`Navbar_ul ${menu === true ? 'active' : ''}`}>
+                        <li className='Nav_items'>
+                            <Link className='Nav_link'>About Us</Link>
+                        </li>
 
-                    <li className='Nav_items'>
-                        <NavDropdown className='Nav_link' title="Our Services" id="navbarScrollingDropdown">
-                            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action4">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action5">
-                                Something else here
-                            </NavDropdown.Item>
-                        </NavDropdown></li>
+                        <li className='Nav_items'>
+                            <NavDropdown className='Nav_link' title="Our Services" id="navbarScrollingDropdown">
+                                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action4">
+                                    Another action
+                                </NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action5">
+                                    Something else here
+                                </NavDropdown.Item>
+                            </NavDropdown></li>
 
-                    <li className='Nav_items'><Link className='Nav_link'>Work with Us</Link></li>
+                        <li className='Nav_items'><Link className='Nav_link'>Work with Us</Link></li>
 
-                    <li className='Nav_items'><Link className='Nav_link'>Blog</Link></li>
-                </ul>
+                        <li className='Nav_items'><Link className='Nav_link'>Blog</Link></li>
+                    </ul>
 
-                <div className='get_touch'>
-                    <Link className='getin_Touch'>Get In Touch</Link>
-                    <RiMenu3Fill className={`menu_icon d-lg-none d-block ${menu === true ? 'd-none':''}`} onClick={() => setmenu(!menu)} />
+                    <div className='get_touch'>
+                        <Link className='getin_Touch'>Get In Touch</Link>
+                        <RiMenu3Fill className={`menu_icon d-lg-none d-block ${menu === true ? 'd-none' : ''}`} onClick={() => setmenu(!menu)} />
 
-                    <MdClose className={`menu_icon d-lg-none d-block ${menu === true? 'd-block':'d-none'}`} onClick={() => setmenu(!menu)} />
+                        <MdClose className={`menu_icon d-lg-none d-block ${menu === true ? 'd-block' : 'd-none'}`} onClick={() => setmenu(!menu)} />
+                    </div>
                 </div>
             </div>
         </div >
