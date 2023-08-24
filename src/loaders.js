dc2f2e9c24d5f6d47e186980dd2ba41f1a882575
harsh <jm@rudraitech.co.in>
2023-08-24 18:08:49 +0530

    24/08/2023

--- src/loaders.js
+++ src/loaders.js
@@ -9,19 +9,27 @@ const Loaders = () => {
     return (
 
         <div className='loadrs_section'>
+
             <div className='loadrs_bg'>
                 <img src={Gradientbg} className='img-fluid' alt="" />
             </div>
 
-            <Vortex
-                visible={true}
-                height="100"
-                width="100"
-                ariaLabel="vortex-loading"
-                wrapperStyle={{}}
-                wrapperClass="vortex-wrapper"
-                colors={['#665DCD', '#5FA4E6', '#D2AB67', '#665DCD', '#5FA4E6', '#D2AB67']}
-            />
+            <div className='d-grid'>
+                <Vortex
+                className="txtss"
+                    visible={true}
+                    height="100"
+                    width="100"
+                    ariaLabel="vortex-loading"
+                    wrapperStyle={{}}
+                    wrapperClass="vortex-wrapper"
+                    colors={['#665DCD', '#5FA4E6', '#D2AB67', '#665DCD', '#5FA4E6', '#D2AB67']}
+                />
+                <div class="txt" id="txt">
+                    <b>G</b><b>R</b><b>A</b><b>V</b><b>I</b><b>T</b><b>Y</b>
+                    &nbsp;  <b>T</b><b>E</b><b>A</b><b>M</b>
+                </div>
+            </div>
         </div>
 
     );
